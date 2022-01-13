190766519e876af2e33dab15ffa447bdf3b725da
Juan Gomes Macário <jgomesmacario@gmail.com>
2022-01-13 12:13:43 -0300

    env secret token

--- src/providers/GenerateTokenProvider.ts
+++ src/providers/GenerateTokenProvider.ts
@@ -2,9 +2,9 @@ import { sign } from "jsonwebtoken";
 
 class GenerateTokenProvider {
     async execute(userId: string) {
-        const token = sign({}, "822f4eaf-f9b2-48e6-8ee8-8f71f92b47ca", {
+        const token = sign({}, process.env.JWT_SECRET, {
             subject: userId,
-            expiresIn: "90000s"
+            expiresIn: "90000000s"
         });
         return token;
     }
